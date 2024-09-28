6d5577d9455a6ecc68b21d92b862e7a8b31fb190
D1093N3 <remidalpe@hotmail.com>
2024-09-28 00:22:49 -0400

    Sept. 27 - functions

--- udemy-complete-js-course/complete-javascript-course-master/10-Functions/starter/script.js
+++ udemy-complete-js-course/complete-javascript-course-master/10-Functions/starter/script.js
@@ -1,1 +1,61 @@
 'use strict';
+
+// Functions Accepting Callback Functions
+
+// How Passing Arguments Works: Value vs. Reference
+/*
+const flight = 'LH234';
+const remi = {
+  name: 'Remi',
+  passport: 1234567890,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 1234567890) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+// checkIn(flight, remi);
+// console.log(flight, remi);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 1000000000);
+};
+newPassport(remi);
+checkIn(flight, remi);
+*/
+
+// Default Parameters
+/*
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  // ES5
+  // numPassengers = numPassengers || 1;
+  // price = price || 199
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+
+createBooking('LH123', undefined, 1000);
+*/
