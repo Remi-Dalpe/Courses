03375238a40ec10240067a67d5cb082fe2bc3921
D1093N3 <remidalpe@hotmail.com>
2024-10-02 03:30:15 -0400

    start bankist

--- udemy-complete-js-course/complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ udemy-complete-js-course/complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -63,7 +63,36 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
-// LECTURES
+//LECTURES///////////////////////////////////////
+
+// PROJECT: "Bankist" App //
+
+//  Looping Arrays: forEach // FOREEACH will always loop entire array
+/*
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+console.log(`==== FOREACH ====`);
+movements.forEach(function (mov, i, arr) {
+  // always needs to be: (current element, current index, entire array we loop over)
+  if (mov > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${mov}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+  }
+});
+
+// forEach With Maps and Sets
+console.log(`==== FOREACH: MAPS & SETS ====`);
 
 const currencies = new Map([
   ['USD', 'United States dollar'],
@@ -71,6 +100,63 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}:${value}`);
+});
 
-/////////////////////////////////////////////////
+// Set
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique);
+currenciesUnique.forEach(function (value, _, map) {
+  console.log(`${value}: ${value}`);
+});
+ */
+
+// The New at Method
+/*
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0)); // Useful if need chaining methods
+// Getting last array element
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+console.log(arr.at(-2));
+console.log('remi'.at(0));
+console.log('remi'.at(-1));
+*/
+
+// Simple Array Methods
+/*
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// SLICE
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2));
+console.log(arr.slice()); // Useful if need chaining methods
+console.log([...arr]);
+
+// SPLICE
+console.log(arr.splice(2));
+arr.splice(-1);
+console.log(arr);
+arr.splice(1, 2);
+console.log(arr);
+
+// REVERSE
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2);
+
+// CONCAT
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]);
+
+// JOIN
+console.log(letters.join(' - '));
+*/
